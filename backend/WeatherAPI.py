123fa05d589788a539b26499213ce3c72b669c9a
YasmineZakaria <yasminezakaria@outlook.com>
2018-11-30 22:53:29 +0200

    New weather api controller methods added

--- backend/WeatherAPI.py
+++ backend/WeatherAPI.py
@@ -1,4 +1,5 @@
 import requests
+from weather import Weather, Unit
 
 
 def api(city):
@@ -7,3 +8,39 @@ def api(city):
     json_data = requests.get(url).json()
     format_add = json_data['main']
     return format_add
+
+
+def apiWeather(city):
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location(city)
+    forecast = location.forecast
+    # Info we can get:-
+    # forecast[0].high
+    # forecast[0].low
+    # forecast[0].text
+    # forecast[0].date
+    # forecast[0].day
+    return forecast[0]
+
+
+def apiWind(city):
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location(city)
+    wind = location.wind
+    # Info we can get:-
+    # wind.direction
+    # wind.speed
+    return wind
+
+
+def apiAtmosphere(city):
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location(city)
+    atmosphere = location.atmosphere
+    # Info we can get:-
+    # atmosphere.humidity
+    # atmosphere.pressure
+    # atmosphere.rising
+    # atmosphere.visibility
+    return atmosphere
+
