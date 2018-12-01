3cae8c214f6d84491609b762ccffae2e16b6a118
YasmineZakaria <yasminezakaria@outlook.com>
2018-12-01 19:03:18 +0200

    Done with Dockerization

--- Server.py
+++ Server.py
@@ -5,17 +5,17 @@ import requests
 
 from backend import WeatherAPI
 
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
 
 app = Flask(__name__)
 Bootstrap(app)
 
+city = ""  # Global variable that contains which city was chosen
+cities = ["Cairo", "Bali", "Paris", "London", "Dubai", "New York", "Bangkok",
+          "Kuala Lumpur"]  # list of cities to choose from
 
-city = ""   #Global variable that contains which city was chosen
-cities = ["Cairo", "Bali", "Paris", "London", "Dubai", "New York", "Bangkok", "Kuala Lumpur"]  #list of cities to choose from
 
-
-@app.route('/')     #homepage
+@app.route('/')  # homepage
 def index():
     global city
     city = ''
@@ -23,18 +23,17 @@ def index():
     return render_template('index.html', main2=' ', main=main, cities=cities, city=city)
 
 
-@app.route('/<string:c>')      #user choose city, but hasn't requested to view current temperature
+@app.route('/<string:c>')  # user choose city, but hasn't requested to view current temperature
 def cityEntered(c):
     main = WeatherAPI.api(c)
     return render_template('index.html', main2=' ', main=main, cities=cities, city=c)
 
 
-
-@app.route('/<string:c>/currentTemp')       #user chose to view current temperature for the selected city
+@app.route('/<string:c>/currentTemp')  # user chose to view current temperature for the selected city
 def currentTemp(c):
     main = WeatherAPI.api(c)
     return render_template('index.html', main=main, main2=main, cities=cities, city=c)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
