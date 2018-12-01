dee0c4f814a08ae527e9ba2cd37c95b1c24683eb
Marina Makram Kamel <marinamakram210@gmail.com>
2018-12-01 12:07:37 +0200

    changing in Server.py

--- Server.py
+++ Server.py
@@ -4,18 +4,19 @@ from flask_bootstrap import Bootstrap
 import requests
 
 from backend import WeatherAPI
+from backend import postgres
 
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
 
 app = Flask(__name__)
 Bootstrap(app)
 
+city = ""  # Global variable that contains which city was chosen
+cities = ["Cairo", "Bali", "Paris", "London", "Dubai", "New York", "Bangkok",
+          "Kuala Lumpur"]  # list of cities to choose from
 
-city = ""   #Global variable that contains which city was chosen
-cities = ["Cairo", "Bali", "Paris", "London", "Dubai", "New York", "Bangkok", "Kuala Lumpur"]  #list of cities to choose from
 
-
-@app.route('/')     #homepage
+@app.route('/')  # homepage
 def index():
     global city
     city = ''
@@ -23,14 +24,15 @@ def index():
     return render_template('index.html', main2=' ', main=main, cities=cities, city=city)
 
 
-@app.route('/<string:c>')      #user choose city, but hasn't requested to view current temperature
+@app.route('/<string:c>')  # user choose city, but hasn't requested to view current temperature
+# retrieve data from database
 def cityEntered(c):
-    main = WeatherAPI.api(c)
-    return render_template('index.html', main2=' ', main=main, cities=cities, city=c)
+    # main = WeatherAPI.api(c)
+    # return render_template('index.html', main2=' ', main=main, cities=cities, city=c)
+    main2 = postgres.showForecastForCity(c)
+    return render_template('index.html', main2=main2, main=' ', cities=cities, city=c)
 
-
-
-@app.route('/<string:c>/currentTemp')       #user chose to view current temperature for the selected city
+@app.route('/<string:c>/currentTemp')  # user chose to view current temperature for the selected city
 def currentTemp(c):
     main = WeatherAPI.api(c)
     return render_template('index.html', main=main, main2=main, cities=cities, city=c)
