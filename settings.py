64c5fb419da1ff211088e24f816da89f9a2b558f
yasminezakaria <yasminezakaria@outlook.com>
2018-12-03 12:15:44 +0200

    edit settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,6 @@
-DATABASES = {
-    'default': {
-        'DATABASE': 'postgres',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',  # to be changed as set in docker-compose.yml (eg. db)
-        'PORT': 5432,  # default postgres port
-        'password': 'marina123'  # to be changed
-    }
-}
-
 DATABASE = 'postgres'
 Name = 'postgres'
 USER = 'postgres'
-HOST = 'localhost'
-PORT = 5432
-PASSWORD = 'marina123'
+HOST = 'db'
+
 
