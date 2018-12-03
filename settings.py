563584bfc7ceb7ab119630957fcebee49538d42b
marinakamel <marinamakram210@gmail.com>
2018-12-03 16:41:40 +0000

    environment var

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-DATABASE = 'postgres'
-Name = 'postgres'
-USER = 'postgres'
-HOST = 'db'
+import os
+DATABASE = os.getenv('POSTGRES_DATABASE','postgres')
+USER = os.getenv('POSTGRES_USER','postgres')
+HOST = os.getenv('POSTGRES_HOST','db')
 
 
