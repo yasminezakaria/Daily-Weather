33f81b2f5a642b18889873c568f3435b5ca2be4b
yasminezakaria <yasminezakaria@outlook.com>
2018-12-03 11:56:30 +0200

    update server.py

--- Server.py
+++ Server.py
@@ -55,4 +55,4 @@ postgres.insertIntoTableWind()
 postgres.insertIntoTableAtmosphere()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+     app.run(host="0.0.0.0", debug=True)
